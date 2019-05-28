3385cc9e1068833fe18c8354be3ef0d8299979a3
hanna chae <hannacne@naver.com>
2019-05-28 20:08:25 +0900

    단일행 함수 DECODE 까지

--- day07.sql
+++ day07.sql
@@ -84,3 +84,153 @@ SELECT '2019-5월-28' + 10 "today + 10일"
 ORA-01722: 수치가 부적합합니다
 01722. 00000 -  "invalid number"
 */
+
+---- 3. to_number() : 오라클이 자동 형변환을 해주므로 
+--                    자주 사용되지 않음.
+SELECT '1000' + 10 계산결과
+  FROM dual
+;
+SELECT to_number('1000') + 10 계산결과
+  FROM dual
+;
+
+/*
+계산결과
+-------
+1010
+*/
+
+----- (5) DECODE(expr, search, result [, search, result] ... [, default])
+/*
+ 만약 default 가 설정되어 있지 않고
+ expr 과 일치하는 search 도 없는 경우
+ DECODE 문의 결과는 NULL 이 된다.
+*/
+--            expr  search1 result1
+SELECT DECODE('YES', 'YES', '입력값이 YES 입니다.'
+                   , 'NO' , '입력값이 NO 입니다.') AS 입력결과
+--                  search2 result2                   
+  FROM dual
+;  
+
+/*
+입력결과
+---------------------
+입력값이 YES 입니다.
+*/
+--            expr  search1 result1
+SELECT DECODE('NO', 'YES', '입력값이 YES 입니다.'
+                  , 'NO' , '입력값이 NO 입니다.') AS 입력결과
+--                  search2 result2                   
+  FROM dual
+; 
+/*
+입력결과
+---------------------
+입력값이 NO 입니다.
+*/
+--            expr  search1 result1
+SELECT DECODE('예', 'YES', '입력값이 YES 입니다.'
+                  , 'NO' , '입력값이 NO 입니다.') AS 입력결과
+--                  search2 result2                   
+  FROM dual
+;
+/*
+입력결과
+-----------
+(null)
+*/
+--            expr  search1 result1
+SELECT DECODE('예', 'YES', '입력값이 YES 입니다.'
+                  , 'NO' , '입력값이 NO 입니다.'
+--                  search2 result2                     
+                  , '입력값이 YES/NO 중 어느 것도 아닙니다.') AS 입력결과
+--                  default                   
+  FROM dual
+;
+/*
+입력결과
+-----------
+입력값이 YES/NO 중 어느 것도 아닙니다.
+*/
+
+-- emp 테이블에서 job(직무) 별로 
+-- 경조사비를 급여대비 일정 비율로 지급하기로 했다.
+-- 지급비율이 다음과 같다고 할 때, 
+-- 각 직원들의 경조사비 지원금을 구해보자.
+-- 사번, 이름, 직무 와 함께 구해보자
+/*
+  CLERK     : 5%
+  SALESMAN  : 4%
+  MANAGER   : 3.7%
+  ANALYST   : 3%
+  PRESIDENT : 1.5%
+*/
+SELECT e.empno
+     , e.ename
+     , e.job
+     , DECODE(e.job, 'CLERK'    , e.sal * 0.05
+                   , 'SALESMAN' , e.sal * 0.04
+                   , 'MANAGER'  , e.sal * 0.037
+                   , 'ANALYST'  , e.sal * 0.03
+                   , 'PRESIDENT', e.sal * 0.015) "경조사 지원금"
+  FROM emp e
+;  
+/*
+EMPNO,  ENAME,  JOB,        경조사 지원금
+-----------------------------------------b
+7369	SMITH	CLERK	    40
+7499	ALLEN	SALESMAN	64
+7521	WARD	SALESMAN	50
+7566	JONES	MANAGER	    110.075
+7654	MARTIN	SALESMAN	50
+7698	BLAKE	MANAGER	    105.45
+7782	CLARK	MANAGER	    90.65
+7839	KING	PRESIDENT	75
+7844	TURNER	SALESMAN	60
+7900	JAMES	CLERK	    47.5
+7902	FORD	ANALYST 	90
+7934	MILLER	CLERK	    65
+9999	J_JAMES	CLERK	
+8888	J%JAMES	CLERK	
+*/
+
+-- 경조사 지원비 결과에 숫자 패턴($999.99) 입히기
+SELECT e.empno
+     , e.ename
+     , e.job
+     , to_char(DECODE(e.job, 'CLERK'    , e.sal * 0.05
+                           , 'SALESMAN' , e.sal * 0.04
+                           , 'MANAGER'  , e.sal * 0.037
+                           , 'ANALYST'  , e.sal * 0.03
+                           , 'PRESIDENT', e.sal * 0.015
+                           , e.sal * 0.010), '$999.99') "경조사 지원금"
+  FROM emp e
+;  
+
+/*
+EMPNO,  ENAME,  JOB,          경조사 지원금
+-------------------------------------
+7369	SMITH	CLERK	      $40.00
+7499	ALLEN	SALESMAN	  $64.00
+7521	WARD	SALESMAN	  $50.00
+7566	JONES	MANAGER	     $110.08
+7654	MARTIN	SALESMAN	  $50.00
+7698	BLAKE	MANAGER	     $105.45
+7782	CLARK	MANAGER	      $90.65
+7839	KING	PRESIDENT	  $75.00
+7844	TURNER	SALESMAN	  $60.00
+7900	JAMES	CLERK	      $47.50
+7902	FORD	ANALYST	      $90.00
+7934	MILLER	CLERK	      $65.00
+9999	J_JAMES	CLERK	
+8888	J%JAMES	CLERK	
+*/
+
+
+
+
+
+
+
+
