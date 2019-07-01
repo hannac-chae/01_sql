b1755b38e353b6bd63b50f55c944bedc1bbc6c2a
hanna chae <hannacne@naver.com>
2019-07-01 15:53:06 +0900

    실습10 과제 추가

--- day12_ddl.sql
+++ day12_ddl.sql
@@ -497,6 +497,41 @@ CREATE TABLE SUB_TABLE3
 -- Table SUB_TABLE3이(가) 생성되었습니다.
 
 
+-- 추가 과제 
+-- 실습 10)
+
+/* 세 개의 테이블을 생성하는 구문을 작성
+   3번 방식으로 작성
+ ------------------------------------------  
+ GAME
+ GAME_CODE  NUMBER(2)     PK       --게임 코드 10, 20, 30, ...
+ GAME_NAME  VARCHAR2(200) NOT NULL --게임 이름 
+ 
+ ------------------------------------------
+ GMEMBER
+ ID         VARCHAR2(4)   PK       --회원의 아이디 M001, M002, ...
+ NAME       VARCHAR2(15)  NOT NULL --회원의 이름   
+  
+ ------------------------------------------
+ MEMBER_GAME_HISTORY
+ ID         VARCHAR2(4)   FK 설정, FK 이름 : FK_ID
+                          GMEMBER 테이블의 ID 컬럼을 참조하도록 설정
+ YEAR       NUMBER(4)     -- 게임을 한 년도
+ GAME_CODE  NUMBER(2)     FK 설정, FK 이름 : FK_GAME_CODE
+                          GAME 테이블의 GAME_CODE 컬럼을 참조하도록 설정
+ ------------------------------------------
+ 
+
+*/
+
+
+
+
+
+
+
+
+
 
 
 
